4dbb27e4b78d1c4d9f7ecfb9521c951c52084050
yaflores <Rio@bushras-MacBook-Pro.local>
2023-04-04 18:21:00 -0400

    add sections

--- components/LocationPicker.js
+++ components/LocationPicker.js
@@ -2,7 +2,9 @@ import styles from './locationpicker.module.scss';
 
 const LocationPicker = () => {
     return <section className={styles.locationPicker}>
-        Location picker goes here 
+        <container>
+            Location picker goes here
+        </container>
     </section>
 }
 export default LocationPicker;
